3cf3d5e57192549a384c24179a5b046cfb10fa40
shivamdewangan <shivamdewangan310@gmail.com>
2025-02-01 22:50:06 +0530

    Update Vite config, package.json and add new assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173, // Ensure the correct port is specified
+    host: '0.0.0.0', // This ensures external access
     proxy: {
       '/api': {
         target: 'https://api.jsonserve.com',
